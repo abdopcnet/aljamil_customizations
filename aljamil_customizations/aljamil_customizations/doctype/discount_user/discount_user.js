15765cc372161ef196142fcd24427c8f2e66cef0
abdopcnet <abdopcnet@gmail.com>
2026-01-18 21:47:33 +0300

    21-47-18-01-2026

---
+++ aljamil_customizations/aljamil_customizations/doctype/discount_user/discount_user.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, abdopcnet@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("discount user", {
+// 	refresh(frm) {
+
+// 	},
+// });
