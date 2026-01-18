3982033f870737bcf186735916b8cfeb720a4624
abdopcnet <abdopcnet@gmail.com>
2026-01-18 21:47:33 +0300

    21-47-18-01-2026

---
+++ aljamil_customizations/public/js/landed_cost_voucher.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2025, Aljamil Customizations
+// For license information, please see license.txt
+
+// Frontend logging: console.log('[landed_cost_voucher.js] method: function_name')
+
+// Custom JavaScript for Landed Cost Voucher
+// This file can be extended with client-side customizations if needed
